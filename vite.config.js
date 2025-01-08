e7eaa6b0c2c8351eb3ccac85f89f8c87e45b0fea
Gaurav Sharma <gauravsharmatheofficial@gmail.com>
2025-01-08 16:01:33 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": "/src",
     },
   },
 });
